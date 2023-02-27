959e51bce0077cd1a15048c69f1b6c7f63378ca2
Dayan <36505543+MicroRJ@users.noreply.github.com>
2023-02-27 18:12:20 +0000

    added sizeof.cc test

--- code/sizeof.cc
+++ code/sizeof.cc
@@ -9,6 +9,33 @@ int main(int _)
   ccassert(sizeof i == 4)
   ccassert(sizeof p == 8)
 
+  int *u[7];
+  int *v[7][7];
+
+  ccassert(sizeof u[0] == 8)
+  ccassert(sizeof v[0][0] == 8)
+
+  ccassert(sizeof u == 7 * sizeof u[0])
+  ccassert(sizeof v[0] == 7 * sizeof v[0][0])
+
+  int x[7];
+  int y[7][7];
+  int z[7][7][7];
+
+  ccassert(sizeof x[0] == 4)
+  ccassert(sizeof y[0][0] == 4)
+  ccassert(sizeof z[0][0][0] == 4)
+
+  ccassert(sizeof x     == 7*4)
+  ccassert(sizeof x[0]  == 4)
+
+  ccassert(sizeof y     == 7*7*4)
+  ccassert(sizeof y[0]  == 7*4)
+
+  ccassert(sizeof z     == 7*7*7*4)
+  ccassert(sizeof z[0]  == 7*7*4)
+
+
   return 1;
 }
 
